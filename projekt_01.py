125fef9c91aaac60a4ecd8bc52a5326af014bdaa
tomasbula <tomas.bula@post.cz>
2025-06-21 20:16:17 +0200

    Pridal jsem 2x print

---
+++ projekt_01.py
@@ -0,0 +1,21 @@
+#import math
+
+#polomery = (1, 2, 3, 4, 5)
+#plochy = list()
+
+#for polomer in polomery:
+#    plocha = round(math.pi * polomer**2, 2)
+#    plochy.append(plocha)
+#print(plochy)
+
+import random
+
+
+
+print("ahoj")
+print("čau")
+print("nazdar")
+
+print("HI")
+
+print("hello")
